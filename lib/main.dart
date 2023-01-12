ff218b16a5c08505990ff697022c02c8f6c1f587
Kawsar Ahmed Shakil <kashakil75@gmail.com>
2023-01-12 15:52:10 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bank_app_ui/transaction_detail_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
